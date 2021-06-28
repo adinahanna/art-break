280cc3557d98bd4326bfa37b87ba33c09d7e4c05
adinahanna <adina.weinberg@gmail.com>
2021-06-27 20:58:13 -0500

    add random number function

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Link } from 'react-router-dom';
+
+const getRandomNum = () => {
+  let randomNum = Math.floor(Math.random() * 1000);
+}
+getRandomNum();
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>hello world</h1>
+    
     </div>
   );
 }
