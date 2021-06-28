edf8ffbb73c7259c23baaff5232a914a1d885ecc
adinahanna <adina.weinberg@gmail.com>
2021-06-28 08:23:04 -0500

    add random number function

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import './App.css';
 import { Route, Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
+let randomNum;
 const getRandomNum = () => {
-  let randomNum = Math.floor(Math.random() * 1000);
+  randomNum = Math.floor(Math.random() * 1000);
 }
 getRandomNum();
 
 function App() {
+  const [artData, setArtData] = useState(null);
+
+  const getArtData = async () => {
+    const url = `https://api.harvardartmuseums.org/image?size=10&page=${randomNum}?apikey=${process.env.API_KEY}`;
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setArtData(data);
+    } catch(err) {
+      console.error(err);
+    }
+  }
+
+  useEffect(() => {
+    getArtData();
+  }, []);
 
   return (
     <div className="App">
